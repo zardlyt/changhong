9375fb567878bb344951123aa0a2824d32d0756f
张雨朦 <yumeng1.zhang@changhong.com>
2017-11-06 16:15:32 +0800

    最新改动

--- src/main/java/com/changhong/cachemanage/controller/CacheController.java
+++ src/main/java/com/changhong/cachemanage/controller/CacheController.java
@@ -12,10 +12,7 @@ import net.sf.json.util.JSONUtils;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
@@ -57,8 +54,8 @@ public class CacheController {
         return RestResultGenerator.genSuccessResult();
     }
 
-    @RequestMapping(value = "/custom/upload")
-    public RestResult upload(@RequestParam(value = "upfile", required=false) MultipartFile file) throws IOException {
+    @RequestMapping(value = "/custom/upload", method = RequestMethod.POST)
+    public RestResult upload(@RequestParam(value = "upfile") MultipartFile file) throws IOException {
         if(file==null) return RestResultGenerator.genErrorResult("请上传文件");
         String name = file.getOriginalFilename();
         long size = file.getSize();
